8a0f5c5159a4720b36243728bdfd63793ff2a1b5
Quanzheng Long <qlong@indeed.com>
2022-12-11 21:40:38 -0800

    Add all samples

---
+++ src/main/java/io/github/cadenceoss/iwf/controller/PersistenceWorkflowController.java
@@ -0,0 +1,34 @@
+package io.github.cadenceoss.iwf.controller;
+
+import io.github.cadenceoss.iwf.core.Client;
+import io.github.cadenceoss.iwf.core.WorkflowStartOptions;
+import io.github.cadenceoss.iwf.workflow.persistence.BasicPersistenceWorkflow;
+import io.github.cadenceoss.iwf.workflow.persistence.BasicPersistenceWorkflowState1;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/persistence")
+public class PersistenceWorkflowController {
+
+    private final Client client;
+
+    public PersistenceWorkflowController(
+            final Client client
+    ) {
+        this.client = client;
+    }
+
+    @GetMapping("/start")
+    public ResponseEntity<String> start() {
+        final String wfId = "basic-query-test-id" + System.currentTimeMillis() / 1000;
+        final WorkflowStartOptions startOptions = WorkflowStartOptions.minimum(10);
+        final String runId = client.startWorkflow(
+                BasicPersistenceWorkflow.class, BasicPersistenceWorkflowState1.STATE_ID, "start", wfId, startOptions);
+        final String output = client.getSimpleWorkflowResultWithWait(String.class, wfId);
+
+        return ResponseEntity.ok(String.format("runId: %s, output: %s", runId, output));
+    }
+}
