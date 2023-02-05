fce42ed092c248cdf86d28fa0bfc8fc48d3434e8
Quanzheng Long <qlong@indeed.com>
2023-02-05 13:21:18 -0800

    fix email message

--- src/main/java/io/iworkflow/workflow/subscription/MyService.java
+++ src/main/java/io/iworkflow/workflow/subscription/MyService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyService {
     public void sendEmail(final String recipient, final String subject, final String content) {
-        System.out.printf("sending an welcome email to %s, title: %s, content: %s %n", recipient, subject, content);
+        System.out.printf("send an email to %s, title: %s, content: %s %n", recipient, subject, content);
     }
 
     public void chargeUser(final String email, final String customerId, final int amount) {
