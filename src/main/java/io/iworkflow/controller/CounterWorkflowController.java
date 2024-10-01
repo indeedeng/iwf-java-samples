5f184e86bfe554f2c84570114716304c99c90aa7
Quanzheng Long <qlong@indeed.com>
2024-10-01 14:03:59 -0700

    Add counter wf

--- src/main/java/io/iworkflow/controller/CounterWorkflowController.java
+++ src/main/java/io/iworkflow/controller/CounterWorkflowController.java
@@ -1,10 +1,7 @@
 package io.iworkflow.controller;
 
 import io.iworkflow.core.Client;
-import io.iworkflow.core.ClientSideException;
-import io.iworkflow.gen.models.ErrorSubStatus;
-import io.iworkflow.workflow.microservices.ImmutableSignupForm;
-import io.iworkflow.workflow.signup.UserSignupWorkflow;
+import io.iworkflow.workflow.update.CounterWorkflow;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,44 +9,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/signup")
-public class SignupWorkflowController {
+@RequestMapping("/counter")
+public class CounterWorkflowController {
 
     private final Client client;
 
-    public SignupWorkflowController(
+    public CounterWorkflowController(
             final Client client
     ) {
         this.client = client;
     }
 
-    @GetMapping("/submit")
+    @GetMapping("/start")
     public ResponseEntity<String> start(
-            @RequestParam String username,
-            @RequestParam String email
+            @RequestParam String id
     ) {
-        try {
-            final ImmutableSignupForm form = ImmutableSignupForm.builder()
-                    .username(username)
-                    .email(email)
-                    .firstName("Test")
-                    .lastName("Test")
-                    .build();
-            client.startWorkflow(UserSignupWorkflow.class, username, 3600, form);
-        } catch (ClientSideException e) {
-            if (e.getErrorSubStatus() != ErrorSubStatus.WORKFLOW_ALREADY_STARTED_SUB_STATUS) {
-                throw e;
-            }
-            return ResponseEntity.ok("username already started registry");
-        }
+        client.startWorkflow(CounterWorkflow.class, id, 3600);
         return ResponseEntity.ok("success");
     }
 
-    @GetMapping("/verify")
-    ResponseEntity<String> verify(
-            @RequestParam String username) {
-        final UserSignupWorkflow rpcStub = client.newRpcStub(UserSignupWorkflow.class, username);
-        String result = client.invokeRPC(rpcStub::verify);
-        return ResponseEntity.ok(result);
+    @GetMapping("/inc")
+    ResponseEntity<Integer> verify(
+            @RequestParam String id) {
+        final CounterWorkflow rpcStub = client.newRpcStub(CounterWorkflow.class, id);
+        return ResponseEntity.ok(client.invokeRPC(rpcStub::inc));
     }
 }
--- src/main/java/io/iworkflow/workflow/update/CounterWorkflow.java
+++ src/main/java/io/iworkflow/workflow/update/CounterWorkflow.java
@@ -5,67 +5,43 @@ import io.iworkflow.core.ObjectWorkflow;
 import io.iworkflow.core.RPC;
 import io.iworkflow.core.StateDef;
 import io.iworkflow.core.communication.Communication;
-import io.iworkflow.core.communication.CommunicationMethodDef;
-import io.iworkflow.core.communication.InternalChannelDef;
 import io.iworkflow.core.persistence.DataAttributeDef;
 import io.iworkflow.core.persistence.Persistence;
 import io.iworkflow.core.persistence.PersistenceFieldDef;
-import io.iworkflow.workflow.MyDependencyService;
-import io.iworkflow.workflow.microservices.SignupForm;
+import io.iworkflow.gen.models.PersistenceLoadingType;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Component
 
-public class UserSignupWorkflow implements ObjectWorkflow {
+public class CounterWorkflow implements ObjectWorkflow {
 
-    public static final String DA_FORM = "Form";
-
-    public static final String DA_Status = "Status";
-    public static final String VERIFY_CHANNEL = "Verify";
-
-    private MyDependencyService myService;
-
-    public UserSignupWorkflow(MyDependencyService myService) {
-        this.myService = myService;
-    }
+    public static final String DA_COUNT = "COUNT";
 
     @Override
     public List<StateDef> getWorkflowStates() {
-        return Arrays.asList(
-                StateDef.startingState(new SubmitState(myService)),
-                StateDef.nonStartingState(new VerifyState(myService))
-        );
+        return new ArrayList<>();
     }
 
     @Override
     public List<PersistenceFieldDef> getPersistenceSchema() {
         return Arrays.asList(
-                DataAttributeDef.create(SignupForm.class, DA_FORM),
-                DataAttributeDef.create(String.class, DA_Status)
+                DataAttributeDef.create(Integer.class, DA_COUNT)
         );
     }
 
-    @Override
-    public List<CommunicationMethodDef> getCommunicationSchema() {
-        return Arrays.asList(
-                InternalChannelDef.create(Void.class, VERIFY_CHANNEL)
-        );
-    }
-
-    // Atomically read/write/send message in RPC
+    // Atomically read/write data attributes in RPC will use Temporal sync update features
     @RPC(
-            dataAttributesLoadingType = P
+            dataAttributesLoadingType = PersistenceLoadingType.PARTIAL_WITH_EXCLUSIVE_LOCK,
+            dataAttributesLockingKeys = {DA_COUNT}
     )
-    public String verify(Context context, Persistence persistence, Communication communication) {
-        String status = persistence.getDataAttribute(DA_Status, String.class);
-        if (status == "verified") {
-            return "already verified";
-        }
-        persistence.setDataAttribute(DA_Status, "verified");
-        communication.publishInternalChannel(VERIFY_CHANNEL, null);
-        return "done";
+    public int inc(Context context, Persistence persistence, Communication communication) {
+        int count = persistence.getDataAttribute(DA_COUNT, Integer.class);
+        count++;
+        persistence.setDataAttribute(DA_COUNT, count);
+        return count;
     }
 }
